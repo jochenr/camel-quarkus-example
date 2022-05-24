c84139ee37f6ca63fe2d01b4943992a3bf7f8502
Riedlinger, Jochen (IT 48) <jochen.riedlinger@l-bank.de>
2022-05-24 16:30:19 +0200

    Initial Project Setup

---
+++ src/main/java/de/jochenr/camelquarkusexample/jms/JmsRouteBuilder.java
@@ -0,0 +1,88 @@
+/**
+ *
+ */
+package de.jochenr.camelquarkusexample.jms;
+
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.util.function.Function;
+
+import org.apache.camel.Exchange;
+import org.apache.camel.LoggingLevel;
+import org.apache.camel.Processor;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.model.TransactedDefinition;
+import org.apache.camel.model.dataformat.JsonLibrary;
+
+import com.fasterxml.jackson.core.JsonParseException;
+
+
+
+/**
+ * @author Jochen Riedlinger
+ *
+ */
+public class JmsRouteBuilder extends RouteBuilder {
+
+	@Override
+	public void configure() throws Exception {
+
+		// @formatter:off
+
+        String logCategory = this.getClass().getCanonicalName() + "." + this.getContext().getName();        
+
+        // Stream Caching global einschalten
+        getContext().setStreamCaching(true);
+        getContext().setAllowUseOriginalMessage(true);
+
+        getContext().setTracing(false);
+		
+		
+		restConfiguration().component("servlet").apiContextPath("apidoc").clientRequestValidation(true);
+
+
+
+        onException(Exception.class)
+	        .handled(true)
+	        .markRollbackOnly()
+	        .setHeader(Exchange.HTTP_RESPONSE_CODE, constant(500))	        
+	        ;
+
+		
+		rest("/jms")
+            .get("/queuetest")
+                .id("queuetest")
+				.produces("text/plain")
+                .to("direct:sendToAMQ");
+
+       
+
+        from("direct:sendToAMQ")
+
+        	.transacted(TransactedDefinition.PROPAGATION_REQUIRED)
+			
+			.removeHeaders("*")
+
+        	.log(LoggingLevel.INFO, logCategory, "\"direct:sendToAMQ\" was called!")
+        	
+
+			//TODO: testweise mal was an die Queue senden:
+			.setBody(constant("Test JMS Message"))
+
+			.to("jmsxa:queue:Meine_Test_XXNMX_Queue?exchangePattern=InOnly&jmsMessageType=Text")
+			.to("jmsxa:queue:Meine_Test_Queue?exchangePattern=InOnly&jmsMessageType=Text")
+
+			.setBody(constant("success"))					
+
+//			.process(ex -> {throw new RuntimeException("absicht");})
+
+        ;
+
+
+
+
+        // @formatter:on
+
+	}
+
+}
---
+++ src/test/java/de/jochenr/camelquarkusexample/jms/JmsResourceTest.java
@@ -0,0 +1,24 @@
+package de.jochenr.camelquarkusexample.jms;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
+
+import org.junit.jupiter.api.Test;
+
+import io.quarkus.test.common.QuarkusTestResource;
+import io.quarkus.test.junit.QuarkusTest;
+
+@QuarkusTest
+@QuarkusTestResource(AmqArtemisTestResource.class)
+public class JmsResourceTest {
+
+    @Test
+    public void testJmsEndpoint() {
+        given()
+          .when().get("/services/jms/queuetest")
+          .then()
+             .statusCode(200)
+             .body(is("success"));
+    }
+
+}
